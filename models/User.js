8e1c1a129e6c155236f4de7b77b360885bc36462
anggaafw <angga_afw@uty.ac.id>
2025-10-09 11:47:51 +0700

    rest get

---
+++ models/User.js
@@ -0,0 +1,52 @@
+module.exports = (sequelize, DataTypes) => {
+  const User = sequelize.define('User', {
+    id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true,
+        allowNull: false,
+    },
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    email: {
+      type: DataTypes.STRING,
+      allowNull: false,
+        unique: true,
+    },
+    pass: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    role: {
+      type: DataTypes.ENUM,
+      values: ['admin', 'operator'],
+      defaultValue: 'operator',
+      allowNull: false
+    },
+    avatar: {
+      type: DataTypes.STRING,
+      allowNull: true   
+    },
+    profession: {
+      type: DataTypes.STRING,
+      allowNull: true
+    },
+    createdAt: {
+      type: DataTypes.DATE,
+      field: 'createdAt',
+    },
+    updatedAt: {
+      type: DataTypes.DATE,
+      field: 'updatedAt',
+      allowNull: true
+    }
+  }
+  , {
+    tableName: 'users',
+    timestamps: true
+  });
+
+  return User;
+};
--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,12 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
+const controller = require('../controller');
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
   res.send('Hallo world ExpressJS');
 });
+
+router.get('/all', controller.getUsers);
 
 module.exports = router;
